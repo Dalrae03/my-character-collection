e4da888044864278678ca82643cd867411f805a9
Yejin Lee <www298@naver.com>
2024-10-06 17:51:36 +0900

    [미션2] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/com/character_collect/mini_project/MiniProjectApplication.kt
@@ -0,0 +1,11 @@
+package com.character_collect.mini_project
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MiniProjectApplication
+
+fun main(args: Array<String>) {
+	runApplication<MiniProjectApplication>(*args)
+}
---
+++ src/test/kotlin/com/character_collect/mini_project/MiniProjectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.character_collect.mini_project
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MiniProjectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
